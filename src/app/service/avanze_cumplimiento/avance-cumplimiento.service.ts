f2fcf5d895271e14101bb36986e776947444f31c
Maria Jose <mjp.ortega@yavirac.edu.ec>
2023-07-14 01:13:34 -0500

    Implementacion boton en proyecto para generar los pdf .

--- src/app/service/avanze_cumplimiento/avance-cumplimiento.service.ts
+++ src/app/service/avanze_cumplimiento/avance-cumplimiento.service.ts
@@ -13,7 +13,7 @@ export class AvanceCumplimientoService {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
   private url = environment.API_URL + '/avanze';
-  private url2 = environment.API_URL + '/project/2';
+  private url2 = environment.API_URL + '/project';
 
 
   public addAvanze(data: any): Observable<any> {
@@ -35,6 +35,13 @@ export class AvanceCumplimientoService {
 
     return this.http.get<any>(
       `${this.url2}/`,
+      this.httpOptions
+    );
+  }
+  public getProyectoById(id:number): Observable<any> {
+
+    return this.http.get<any>(
+      `${this.url2}/${id}`,
       this.httpOptions
     );
   }
