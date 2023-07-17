874f065c8c26edc31e410ba29a2b874ce3ce98b1
Maria Jose <mjp.ortega@yavirac.edu.ec>
2023-07-16 23:27:01 -0500

    Implemenacion informe_inicial y informe final

--- src/app/feature/docente-vinculacion/formulario-final-tutor/formulario-final-tutor.component.ts
+++ src/app/feature/docente-vinculacion/formulario-final-tutor/formulario-final-tutor.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { jsPDF } from "jspdf";
+import 'jspdf-autotable';
+import { ImageConstants } from 'src/app/constanst/ImageConstants';
 @Component({
   selector: 'app-formulario-final-tutor',
   templateUrl: './formulario-final-tutor.component.html',
   styleUrls: ['./formulario-final-tutor.component.css']
 })
-export class FormularioFinalTutorComponent {
+export class FormularioFinalTutorComponent implements OnInit {
+
+  public doc: any;
+
+
+  ngOnInit() {
+
+  }
+
+  /* pdf proyecto*/
+  public Generar_Solicitud() {
+    this.doc = new jsPDF('p', 'pt');
+    this.doc.addImage(ImageConstants.fondo_pdf, 'JPG', 0, 0, 595, 842);
+    this.doc.setFontSize(14);
+    // this.doc.setFontStyle('bold');
+    this.doc.setFont("Roboto", 'bold');
+    this.doc.text('INSTITUTO SUPERIOR TECNOLÓGICO', 175, 140);
+    this.doc.text('D E  T U R I S M O   Y  P A T R I M O N I O', 155, 160);
+    this.doc.text('“Y A V I R A C”', 250, 175);
+    this.doc.setFontSize(12);
+    this.doc.text('Informe de seguimiento y/o final de actividades de vinculación con la comunidad', 95, 210);
+
+
+    this.doc.setFontSize(9);
+    this.doc.line(45, 700, 200, 700);
+    this.doc.text('TUTOR PROYECTO', 65, 710);
+    this.doc.text('$F{Tutor}', 75, 725, { maxWidth: 100, align: 'center' });
+
+    this.doc.line(250, 700, 375, 700);
+    this.doc.text('TUTOR ENTIDAD BENEFICIARIA', 240, 710);
+    this.doc.text('$F{rector}', 295, 725, { maxWidth: 100, align: 'center' });
+
+    this.doc.line(450, 700, 550, 700);
+    this.doc.text('ESTUDIANTE', 470, 710,);
+    this.doc.text('$F{rector}', 475, 725, { maxWidth: 100, align: 'center' });
+    this.doc.autoTable({
+      html: '#table', startY: 255,
+    })
+    this.doc.autoTable({
+      html: '#table2', startY: 355,
+    })
+    this.doc.save("Informae_final.pdf");
+    // location.reload();
+  }
+
 
 }
--- src/app/feature/docente-vinculacion/informe-inicial/informe-inicial/informe-inicial.component.ts
+++ src/app/feature/docente-vinculacion/informe-inicial/informe-inicial/informe-inicial.component.ts
@@ -1,10 +1,125 @@
-import { Component } from '@angular/core';
-
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { jsPDF } from "jspdf";
+import 'jspdf-autotable';
+import { ImageConstants } from 'src/app/constanst/ImageConstants';
 @Component({
   selector: 'app-informe-inicial',
   templateUrl: './informe-inicial.component.html',
   styleUrls: ['./informe-inicial.component.css']
 })
-export class InformeInicialComponent {
+export class InformeInicialComponent implements OnInit {
+  docente: FormGroup;
+  public doc: any;
+
+  constructor(
+
+    private miDatePipe: DatePipe
+
+  ) { }
+
+  ngOnInit() {
+    this.docente = new FormGroup({
+      persona: new FormControl(''),
+      asunto: new FormControl(''),
+      fecha: new FormControl(''),
+      name_docente: new FormControl(''),
+      ci_docente: new FormControl(''),
+      carrera: new FormControl(''),
+      name_proyect: new FormControl(''),
+      start_vinculacion: new FormControl(''),
+      comments: new FormControl(''),
+
+    });
+  }
+
+
+
+
+  /* pdf proyecto*/
+  public Generar_Solicitud() {
+    console.log(this.docente.value);
+
+
+    this.doc = new jsPDF('p', 'pt');
+    const pageContent = () => {
+      // HEADER
+
+      this.doc.setFont("Roboto", 'bold');
+      this.doc.setFontSize(11);
+      this.doc.text('A:', 45, 210);
+      this.doc.setFont("Roboto-Regular-normal.ttf", "Roboto-Regular", "normal");
+      this.doc.text(this.docente.value.persona, 215, 210);
+      this.doc.setFont("Roboto", 'bold');
+      this.doc.text('Coordinadora de Carrera', 215, 225);
+
+
+      this.doc.setFontSize(11);
+      this.doc.setFont("Roboto", 'bold');
+      this.doc.text('DE:', 45, 255);
+      this.doc.setFontSize(11);
+      this.doc.setFont("Roboto-Regular-normal.ttf", "Roboto-Regular", "normal");
+      this.doc.text(this.docente.value.name_docente, 215, 255, { maxWidth: 250, align: 'justify' });
+      this.doc.setFont("Roboto", 'bold');
+      this.doc.text('Tutor de Proyecto', 215, 265, { maxWidth: 250, align: 'justify' });
+
+
+      this.doc.setFontSize(11);
+      this.doc.setFont("Roboto", 'bold');
+      this.doc.text('ASUNTO:', 45, 295);
+      this.doc.setFontSize(11);
+      this.doc.setFont("Roboto-Regular-normal.ttf", "Roboto-Regular", "normal");
+      this.doc.text(this.docente.value.asunto, 215, 295);
+
+
+      this.doc.setFontSize(11);
+      this.doc.setFont("Roboto", 'bold');
+      this.doc.text('PROYECTO:', 45, 325);
+      this.doc.setFontSize(11);
+      this.doc.setFont("Roboto-Regular-normal.ttf", "Roboto-Regular", "normal");
+      this.doc.text(this.docente.value.name_proyect, 215, 325, { maxWidth: 250, align: 'justify' });
+
+
+
+      this.doc.setFontSize(11);
+      this.doc.setFont("Roboto", 'bold');
+      this.doc.text('FECHA:', 45, 355);
+      this.doc.setFontSize(11);
+      this.doc.setFont("Roboto-Regular-normal.ttf", "Roboto-Regular", "normal");
+      this.doc.text(this.docente.value.fecha, 215, 355, { maxWidth: 250, align: 'justify' });
+
+      const fechaFormateada = this.miDatePipe.transform(this.docente.value.fecha, "'Quito,' d 'de' MMMM 'del' yyyy");
+
+      this.doc.setFontSize(12);
+      this.doc.setFont("Roboto-Regular-normal.ttf", "Roboto-Regular", "normal");
+      this.doc.text('Yo ' + this.docente.value.name_docente + ' con CI: ' + this.docente.value.ci_docente + ' docente de la carrera ' + this.docente.value.carrera + ', tengo bien a informar que,' +
+        'siguiendo con el cronograma de actividades establecido en el proyecto vinculación ' + this.docente.value.name_proyect + ', le informó que el día de hoy ' + fechaFormateada + ', se procedió ' +
+        'a dar inicio con el desarrollo del mismo.', 45, 455, { maxWidth: 500, align: 'justify' });
+
+
+      this.doc.setFontSize(11);
+      this.doc.setFont("Roboto", 'bold');
+      this.doc.text('ATENTAMENTE', 45, 660);
+      this.doc.setFont("Roboto-Regular-normal.ttf", "Roboto-Regular", "normal");
+      this.doc.text(this.docente.value.name_docente, 45, 675);
+
+    }
+    this.doc.addImage(ImageConstants.fondo_pdf, 'JPG', 0, 0, 595, 842);
+
+
+
+
+
+
+    this.doc.autoTable({
+      addPageContent: pageContent,
+
+    })
+
+    this.doc.save("Solicitud.pdf");
+    location.reload();
+  }
+
 
 }
