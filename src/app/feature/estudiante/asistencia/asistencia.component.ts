713ac490c043ec918872c2227226a4f641696627
Jose Alpala <alpalajose@gmail.com>
2023-08-21 01:44:14 -0500

    feat-attendance: listar asistencias

---
+++ src/app/feature/estudiante/asistencia/asistencia.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import esLocale from '@fullcalendar/core/locales/es';
+
+@Component({
+  selector: 'app-asistencia',
+  templateUrl: './asistencia.component.html',
+  styleUrls: ['./asistencia.component.css']
+})
+export class AsistenciaComponent implements OnInit {
+
+  calendarOptions: CalendarOptions = {
+    locale: esLocale,
+    plugins: [dayGridPlugin],
+    initialView: 'dayGridMonth',
+    weekends: false,
+    events: [
+      { title: 'Meeting', start: new Date() }
+    ]
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
